22d5d72bc39cbb3d1e152bac264e1c2ab64e6169
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-12 20:27:50 +0000

    Fixed the sizes of the charts

--- app/views/reports/loginsTotal.php
+++ app/views/reports/loginsTotal.php
@@ -10,11 +10,15 @@
 <div style="display: flex; align-items: center; justify-content: center;"> 
 <h1>Total logins by username report</h1>
 </div>
+<div style="display: flex; justify-content: center;">
+  <div style="width: 2000px; height 2000px;">
 <canvas id="loginsTotalChart"></canvas>
+  </div>
+</div>
 <script>
   const ctx = document.getElementById('loginsTotalChart');
   chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'doughnut',
     data: {
       labels: <?php echo json_encode(array_column($data['logins'], 'username')); ?>,
       datasets: [{
@@ -24,11 +28,8 @@
       }]
     },
     options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
+      maintainAspectRatio: false,
+      reponsive: true
     }
   });
 </script>
--- app/views/reports/mostRemindersPerson.php
+++ app/views/reports/mostRemindersPerson.php
@@ -10,7 +10,11 @@
 <div style="display: flex; align-items: center; justify-content: center;"> 
 <h1>User with the most reminders report</h1>
 </div>
-<canvas id="mostRemindersPersonChart"></canvas>
+<div style="display: flex; justify-content: center;">
+  <div style="width: 500px; height: 500px;">
+    <canvas id="mostRemindersPersonChart"></canvas>
+</div>
+</div>
 <script>
   const ctx = document.getElementById('mostRemindersPersonChart');
   chart = new Chart(ctx, {
@@ -24,6 +28,8 @@
           }]
     },
     options: {
+        maintainAspectRatio: false,
+        reponsive: true,
         scales: {
           y: {
             beginAtZero: true
