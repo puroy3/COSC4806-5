960b408a0fec4b2b860662aa813755dc825e357b
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-12 18:20:53 +0000

    Added chart for total logins

--- app/views/reports/loginsTotal.php
+++ app/views/reports/loginsTotal.php
@@ -1,4 +1,22 @@
 <canvas id="loginsTotalChart"></canvas>
 <script>
-  var ctx = 
+  const ctx = document.getElementById('loginsTotalChart');
+  chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: <?php echo json_encode(array_column($data['logins'], 'username')); ?>,
+      datasets: [{
+        label: 'Total Logins by Username',
+        data: <?php echo json_encode(array_column($data['logins'], 'login_count')); ?>,
+        borderWidth: 1
+      }]
+    },
+    options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+    }
+  });
 </script>
