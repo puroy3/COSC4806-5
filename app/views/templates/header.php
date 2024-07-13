4a0dcb797197207e9b644efaa9347cc555b76937
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-13 02:52:19 +0000

    Added a css file.

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -7,6 +7,7 @@ if (!isset($_SESSION['auth'])) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
+      <link rel="stylesheet" href="/css/styles.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <link rel="icon" href="/favicon.png">
--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -7,6 +7,8 @@ if (isset($_SESSION['auth']) == 1) {
 
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <link rel="stylesheet" href="/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="/favicon.png">
     <title>COSC 4806</title>
