1224a2fe93479f73458a749bb042fc591e28016e
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-13 00:18:56 +0000

    Changed text that is displayed in Home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h2 class="display-5 text-center">Assignment 5</h2>
+                <h2 class="display-5 text-center">Home</h2>
                 <h1> Welcome, <?=$_SESSION['username'] ?></h1>
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
             </div>
